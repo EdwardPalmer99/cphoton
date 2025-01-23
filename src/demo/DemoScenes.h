59a0706978afa8fde755f75d88ea0835691596de
Edward Palmer <edward.palmer@ukaea.uk>
2025-01-23 23:01:30 +0000

    Moves demo scenes into new file.

---
+++ src/demo/DemoScenes.h
@@ -0,0 +1,13 @@
+/**
+ * @file DemoScenes.h
+ * @author Edward Palmer
+ * @date 2025-01-23
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#pragma once
+
+void renderBatCave(const char *path);
+void renderMengerCubes(const char *path);
