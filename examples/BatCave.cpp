b220939618872801df11cda4a601561db4e2f92e
Edward Palmer <{username}@users.noreply.github.com>
2025-03-02 19:08:05 +0000

    Update batcave example (not working).

--- examples/BatCave.cpp
+++ examples/BatCave.cpp
@@ -9,11 +9,19 @@
 
 extern "C"
 {
-#include "CPhoton.h"
+#include "engine/RenderSettings.h"
+#include "utility/PPMWriter.h"
 }
 
 #include "engine/PhotonEngine.hpp"
+#include "engine/Scene.hpp"
+#include "engine/primitives/BVHNode.hpp"
+#include "engine/primitives/Cube.hpp"
+#include "engine/primitives/Plane.hpp"
+#include "engine/primitives/Primitive.hpp"
+
 #include <cstdlib>
+#include <vector>
 
 Primitive *makeDarkKnightRoom(double length, double width, double height);
 
@@ -27,31 +35,27 @@ int main(int argc, const char *argv[])
 
     Primitive *room = makeDarkKnightRoom(20, 16.0, 5);
 
-    Scene *scene = makeScene();
-
-    scene->addObject(scene, room);
+    Scene scene;
+    scene.addObject(room);
 
     // Monolith:
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 9; j++)
         {
-            Primitive *cube = makeCube(point3(0.5 * i, 0.25 + 0.5 * j, 0), zeroVector(), 0.5,
+            Primitive *cube = new Cube(point3(0.5 * i, 0.25 + 0.5 * j, 0), zeroVector(), 0.5,
                                        makeLambertian(makeSolidTexture(color3(.01, .01, .01))));
 
-            scene->addObject(scene, cube);
+            scene.addObject(cube);
         }
     }
 
-    scene->markAsFinished(scene);
-
     PhotonEngine engine(gRenderSettings.pixelsWide, gRenderSettings.pixelsHigh);
 
-    PPMImage *outputImage = engine.render(scene, &camera);
+    PPMImage *outputImage = engine.render(&scene, &camera);
 
     writeBinary16BitPPMImage(outputImage, gRenderSettings.outputPath);
 
-    scene->destructor(scene);
     freePPMImage(outputImage);
 
     return 0;
@@ -66,48 +70,30 @@ Primitive *makeDarkKnightRoom(double length, double width, double height)
     Material *wallMaterial = makeLambertian(makeSolidTexture(color3(0.05, 0.05, 0.05)));
     Material *lightMaterial = makeEmitter(color3(.9, .9, .9));
 
-    int numObjects = 0;
-    int objectCapacity = 100;
+    std::vector<Primitive *> objects;
 
-    Primitive **objects = (Primitive **)malloc(sizeof(Primitive *) * objectCapacity);
-    if (!objects) return NULL;
-
-    objects[numObjects++] = makePlane(point3(halfRoomW, 0, 0), vector3(-1, 0, 0), wallMaterial);
-    objects[numObjects++] = makePlane(point3(-halfRoomW, 0, 0), vector3(1, 0, 0), wallMaterial);
-    objects[numObjects++] = makePlane(point3(0, 0, -halfRoomL), vector3(0, 0, 1), wallMaterial);
-    objects[numObjects++] = makePlane(point3(0, 0, halfRoomL), vector3(0, 0, -1), wallMaterial);
-    objects[numObjects++] = makePlane(point3(0, height, 0), vector3(0, -1, 0), wallMaterial);
-    objects[numObjects++] = makePlane(point3(0, 0, 0), vector3(0, 1, 0), wallMaterial);
+    objects.push_back(new Plane(point3(halfRoomW, 0, 0), vector3(-1, 0, 0), wallMaterial));
+    objects.push_back(new Plane(point3(-halfRoomW, 0, 0), vector3(1, 0, 0), wallMaterial));
+    objects.push_back(new Plane(point3(0, 0, -halfRoomL), vector3(0, 0, 1), wallMaterial));
+    objects.push_back(new Plane(point3(0, 0, halfRoomL), vector3(0, 0, -1), wallMaterial));
+    objects.push_back(new Plane(point3(0, height, 0), vector3(0, -1, 0), wallMaterial));
+    objects.push_back(new Plane(point3(0, 0, 0), vector3(0, 1, 0), wallMaterial));
 
     // Create all of the lights on the ceiling:
     for (int i = -halfRoomW; i <= halfRoomW; i += 1)
     {
         for (int j = -halfRoomL; j <= halfRoomL; j += 1)
         {
-            Primitive *floorPanel = makeCube(point3(i + 0.495, -0.490, j + 0.495), zeroVector(), 0.99, wallMaterial);
+            Primitive *floorPanel = new Cube(point3(i + 0.495, -0.490, j + 0.495), zeroVector(), 0.99, wallMaterial);
+
             Primitive *ceilingPanel =
-                makeCube(point3(i + 0.495, height + .494, j + 0.495), zeroVector(), 0.99, lightMaterial);
+                new Cube(point3(i + 0.495, height + .494, j + 0.495), zeroVector(), 0.99, lightMaterial);
 
-            if (numObjects > 0.8 * objectCapacity)
-            {
-                void *reallocPtr = realloc(objects, sizeof(Primitive *) * objectCapacity * 2);
-                if (!reallocPtr)
-                {
-                    free(objects);
-                    return NULL;
-                }
-
-                objects = (Primitive **)reallocPtr;
-                objectCapacity *= 2;
-            }
-
-            objects[numObjects++] = floorPanel;
-            objects[numObjects++] = ceilingPanel;
+            objects.push_back(floorPanel);
+            objects.push_back(ceilingPanel);
         }
     }
 
-    Primitive *roomNode = makeBVHNode(objects, 0, numObjects);
-
-    free(objects);
-    return roomNode;
+    objects.shrink_to_fit();
+    return new BVHNode(objects.data(), 0, objects.size());
 }
