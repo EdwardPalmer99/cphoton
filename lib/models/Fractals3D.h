2e229c1f06f61a8e4cecf35edba58c90f8426c5a
Edward Palmer <{username}@users.noreply.github.com>
2025-03-02 19:08:05 +0000

    Comments-out Fractals3D, PolyhedralModels (will need to be rewritten).

--- lib/models/Fractals3D.h
+++ lib/models/Fractals3D.h
@@ -1,23 +1,23 @@
-//
-//  Fractals3D.h
-//  CPhoton
-//
-//  Created by Edward on 05/02/2023.
-//
+// //
+// //  Fractals3D.h
+// //  CPhoton
+// //
+// //  Created by Edward on 05/02/2023.
+// //
 
-#ifndef Fractals3D_h
-#define Fractals3D_h
+// #ifndef Fractals3D_h
+// #define Fractals3D_h
 
-#include "engine/HitRec.h"
-#include "engine/Material.h"
-#include "engine/Primitive.h"
-#include "engine/Ray.h"
-#include "utility/PPMWriter.h"
-#include <stdlib.h>
+// #include "engine/HitRec.h"
+// #include "engine/Material.h"
+// #include "engine/Primitive.h"
+// #include "engine/Ray.h"
+// #include "utility/PPMWriter.h"
+// #include <stdlib.h>
 
-struct fractal_t;
+// struct fractal_t;
 
-struct primitive_t *makeMandelbulbFractal(Point3 center, Material *material);
-struct primitive_t *makeQuaternionFractal(Point3 center, Material *material);
+// struct primitive_t *makeMandelbulbFractal(Point3 center, Material *material);
+// struct primitive_t *makeQuaternionFractal(Point3 center, Material *material);
 
-#endif /* Fractals3D_h */
+// #endif /* Fractals3D_h */
