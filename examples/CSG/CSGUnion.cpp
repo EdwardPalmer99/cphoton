12ca11e67f7424e159dbf0094c0b446368f5b208
Edward Palmer <{username}@users.noreply.github.com>
2025-03-24 14:44:35 +0000

    Updates CSG examples with two cubes

--- examples/CSG/CSGUnion.cpp
+++ examples/CSG/CSGUnion.cpp
@@ -23,14 +23,13 @@ int main(int argc, const char *argv[])
     // Create the camera:
     Camera camera(45.0, RenderSettings::instance().aspectRatio(), 1, 0, point3(-2, 3, 4), point3(0, 1, 0));
 
-    Primitive *cube1 = new Cube(point3(0.5, 1, 0), vector3(22.5, 0, 0), 1,
-                                std::make_shared<MetalMaterial>(std::make_shared<SolidTexture>(color3(0, 1, 0))));
-    Primitive *cube2 = new Cube(point3(0, 1, 0), vector3(-22.5, 0, 0), 1,
-                                std::make_shared<MetalMaterial>(std::make_shared<SolidTexture>(color3(1, 0, 0))));
+    Primitive *cube1 =
+        new Cube(point3(0.5, 1, 0), vector3(22.5, 0, 0), 1, std::make_shared<MetalMaterial>(color3(0, 1, 0)));
+    Primitive *cube2 =
+        new Cube(point3(0, 1, 0), vector3(-22.5, 0, 0), 1, std::make_shared<MetalMaterial>(color3(1, 0, 0)));
 
     Primitive *plane =
-        new Plane(point3(0, 0, 0), point3(0, 1, 0),
-                  std::make_shared<MatteMaterial>(std::make_shared<SolidTexture>(color3(0.1, 0.1, 0.1))));
+        new Plane(point3(0, 0, 0), point3(0, 1, 0), std::make_shared<MatteMaterial>(color3(0.1, 0.1, 0.1)));
 
     Primitive *CSG = new CSGNode(cube1, cube2, CSGNode::CSGUnion);
 
